41fe59c85f57836f00fab87936d22529b9f32cf0
Zhantore <zhantore29@gmail.com>
2026-03-26 12:43:32 +0500

    feat: complete auth logic (register/login) and fix compiler errors

---
+++ go.mod
@@ -0,0 +1,9 @@
+module bookify
+
+go 1.25.6
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/lib/pq v1.12.0 // indirect
+	golang.org/x/crypto v0.49.0 // indirect
+)
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,1 +1,21 @@
 package domain
+
+import "time"
+
+
+type Role string
+
+const (
+    RoleClient   Role = "client"
+    RoleProvider Role = "provider"
+    RoleAdmin    Role = "admin"
+)
+
+
+type User struct {
+    ID           int       `json:"id"`
+    Email        string    `json:"email"`
+    PasswordHash string    `json:"-"` 
+    Role         Role      `json:"role"`
+    CreatedAt    time.Time `json:"created_at"`
+}
