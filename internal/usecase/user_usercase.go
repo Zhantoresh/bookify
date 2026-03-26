b9db6be1f52db2949d05fd57a8b04c82894ab0f4
Zhantoresh <z_orazymbetov@kbtu.kz>
2026-03-26 12:43:32 +0500

    feat: complete auth logic (register/login) and fix compiler errors

--- internal/usecase/user_usercase.go
+++ internal/usecase/user_usercase.go
@@ -1,1 +1,60 @@
 package usecase
+
+import (
+	"bookify/internal/database"
+	"bookify/internal/domain" 
+	"bookify/pkg/auth"
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserUsecase struct {
+	repo *database.UserRepository
+}
+
+func NewUserUsecase(repo *database.UserRepository) *UserUsecase {
+	return &UserUsecase{repo: repo}
+}
+
+
+func (u *UserUsecase) Register(email, password string, role domain.Role) (*domain.User, error) {
+	hashedPassword, err := u.HashPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &domain.User{
+		Email:        email,
+		PasswordHash: hashedPassword,
+		Role:         role,
+	}
+
+	if err := u.repo.CreateUser(user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *UserUsecase) HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func (u *UserUsecase) ComparePassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func (u *UserUsecase) Login(email, password string) (string, error) {
+	user, err := u.repo.GetByEmail(email)
+	if err != nil {
+		return "", err
+	}
+
+	if !u.ComparePassword(password, user.PasswordHash) {
+		return "", errors.New("invalid credentials")
+	}
+
+	return auth.GenerateToken(user.ID, string(user.Role))
+}
